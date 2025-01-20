1795d47df19e3459dc9ea71a197d21a0df5b72d0
imrangit07 <imranhussain21309@gmail.com>
2025-01-20 14:55:03 +0530

    update misssing

--- array/missing.cpp
+++ array/missing.cpp
@@ -1,37 +1,19 @@
-// #include <iostream>
-// using namespace std;
-
-// int main()
-// {
-//     int arr[] = {1, 2, 4, 6, 7};
-
-//     for (int i = 0; i < 4; i++)
-//     {
-//         if (arr[i + 1] - arr[i] > 1)
-//         {
-//             cout << arr[i] + 1 << " ";
-//         }
-//     }
-
-//     return 0;
-// }
-
-
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int arr[] = {10, 20, 22, 23, 28};
+    int arr[] = {1, 2, 4, 6, 7, 8};
+    int n=sizeof(arr)/sizeof(arr[0]);
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i + 1] - arr[i] > 1)
         {
-            int start=arr[i]+1;
-            while(start<arr[i+1]){
-                cout<<start++<<" ";
+            int gap = arr[i] + 1;
+            while (gap < arr[i + 1])
+            {
+                cout << gap++ << " ";
             }
         }
     }

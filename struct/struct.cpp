b8f80b117a0d72e2f42b13ec35c72471ac953482
imrangit07 <imranhussain21309@gmail.com>
2025-01-23 15:29:53 +0530

    Add structer

---
+++ struct/struct.cpp
@@ -0,0 +1,40 @@
+// #include<iostream>
+// using namespace std;
+
+// struct str
+// {
+//    string name="imran";
+//    int age=23;
+// };
+ 
+// int main(){
+//     str n;
+//     n.name="Hussain";
+//     cout<<n.name<<endl<<n.age;
+//     return 0;
+// }
+
+// Pointer to structure
+
+// #include<iostream>
+// using namespace std;
+
+// struct str
+// {
+//    string name;
+//    int age=23;
+// };
+ 
+// int main(){
+//     str a;
+//     str *n=&a;
+//     a.name="Imran";
+
+//     cout<<n->name<<endl<<a.age;
+ 
+   
+//     return 0;
+// }
+
+
+// Self refential structure

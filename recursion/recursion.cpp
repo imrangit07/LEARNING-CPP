2150abc4cee2eb70df81035af86a940a3808ab45
imrangit07 <imranhussain21309@gmail.com>
2025-02-09 23:49:32 +0530

    Add update in recursion file

--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -6,12 +6,16 @@ if(n<=1){
     return 1;
 }
 return n* fact(n-1);
+//n*fact(4)
+//5*4*fact(3)
+//5*4*3*fact(2)
+//5*4*3*2*fact(1)
 }
  
 int main(){
-    int n;
-    cout<<"Enter number: ";
-    cin>>n;
+    int n=5;
+    // cout<<"Enter number: ";
+    // cin>>n;
     cout<<fact(n);
     return 0;
 }
--- revers/revers.cpp
+++ revers/revers.cpp
@@ -1,17 +1,44 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int num = 1234;
+
+//     int digit1, digit2, digit3;
+//     digit1 = num % 10;
+//     num = num / 10;
+//     digit2 = num % 10;
+//     num = num / 10;
+//     digit3 = num % 10;
+//     num = num / 10;
+//     cout << digit1 << digit2 <<digit3 << num;
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
+int Rev = 0;
+int rev(int num)
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        Rev = (Rev * 10) + (num % 10);
+        num = num / 10;
+    }
+    return Rev;
+}
+void second(int num2)
+{
+    rev(num2);
+}
 
 int main()
 {
-    int num = 1234;
 
-    int digit1, digit2, digit3;
-    digit1 = num % 10;
-    num = num / 10;
-    digit2 = num % 10;
-    num = num / 10;
-    digit3 = num % 10;
-    num = num / 10;
-    cout << digit1 << digit2 <<digit3 << num;
+    int num = 1234;
+    int num2 = 6789;
+    second(num2);
+    cout<<rev(num);
     return 0;
 }
